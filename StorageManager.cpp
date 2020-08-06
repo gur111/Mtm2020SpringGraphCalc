a19c48479dcb434f5b8fb136bbdd4afd7e1c3803
Gur Telem <gur111telem@gmail.com>
2020-08-06 16:26:30 +0300

    Implement reset, who and remove in StorageManager

--- StorageManager.cpp
+++ StorageManager.cpp
@@ -3,13 +3,26 @@
 using std::shared_ptr;
 
 namespace GraphCalc {
-shared_ptr<Graph> StorageManager::set(std::string name, shared_ptr<Graph> graph) {
+void StorageManager::who() {
+    for (auto graph : vars) {
+        std::cout << graph.first;
+    }
+}
+void StorageManager::reset() { this->vars.clear(); }
+shared_ptr<Graph> StorageManager::set(std::string name,
+                                      shared_ptr<Graph> graph) {
     return vars[name] = graph;
+}
+void StorageManager::remove(std::string name) {
+    if (this->vars.erase(name) == 0) {
+        // TODO: Throw error, element doesn't exist
+    }
 }
 
 shared_ptr<Graph> StorageManager::get(std::string name) {
     if (vars.find(name) == vars.end()) {
         // TODO: Throw exception var not defined
+        return nullptr;
     } else {
         return vars[name];
     }
--- StorageManager.h
+++ StorageManager.h
@@ -13,8 +13,14 @@ class StorageManager {
     std::map<std::string, std::shared_ptr<Graph>> vars;
 
    public:
+    /**
+     * Clears the map
+     */
+    void reset();
+    void who();
     std::shared_ptr<Graph> set(std::string name, std::shared_ptr<Graph> graph);
     std::shared_ptr<Graph> get(std::string name);
+    void remove(std::string name);
 };
 }  // namespace GraphCalc
 
