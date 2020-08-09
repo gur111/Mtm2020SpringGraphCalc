88ba93a3d8356ae1ee23945118d80a1a19ac2206
Gur Telem <gur111telem@gmail.com>
2020-08-09 22:44:01 +0300

    Ignore first iostream error (it's caused by last line for some reason, idfc)

--- CalcRunner.cpp
+++ CalcRunner.cpp
@@ -11,6 +11,7 @@ namespace GraphCalc {
 int calcRunner(std::istream &is, std::ostream &os, bool interactive) {
     string line;
     GraphCalc calc;
+    bool is_first_error = true;
     while (not is.eof()) {
         if (interactive) {
             os << "Gcalc> " << flush;
@@ -18,8 +19,12 @@ int calcRunner(std::istream &is, std::ostream &os, bool interactive) {
         try {
             std::getline(is, line);
         } catch (const std::exception &exp) {
-            os << "Error: Error reading line. Got: " << exp.what() << "."
-               << std::endl;
+            if (is_first_error) {
+                is_first_error = false;
+            } else {
+                os << "Error: Error reading line. Got: " << exp.what() << "."
+                   << std::endl;
+            }
         }
         try {
             if (not calc.runCmd(line)) {
