f8835d07f1f3f27460f5ca99ba3a7b99c48bd772
Gur Telem <gur111telem@gmail.com>
2020-08-10 17:04:41 +0300

    Move exception catching to GraphSwig.cpp instead of graph.i to allow graph.i to compile as c++

--- GraphSwig.cpp
+++ GraphSwig.cpp
@@ -1,78 +1,133 @@
 #include "GraphSwig.h"
+#include "Graph.h"
 
 #include <iostream>
 
 using namespace GraphCalc;
 
-Graph *create() { return new Graph(); }
+Graph *create() {
+    try {
+        return new Graph();
+    } catch (const std::exception &excpt) {
+        std::cout << excpt.what() << std::endl;
+        return NULL;
+    }
+}
 
 void disp(Graph *graph) {
-    if (graph == nullptr) {
-        throw Missing("Input graph cannot be a nullptr.");
+    try {
+        if (graph == nullptr) {
+            throw Missing("Input graph cannot be a nullptr.");
+        }
+        std::cout << *graph;
+    } catch (const std::exception &excpt) {
+        std::cout << excpt.what() << std::endl;
     }
-    std::cout << *graph;
 }
 
 void destroy(Graph *graph) {
-    if (graph == nullptr) {
-        throw Missing("Input graph cannot be a nullptr.");
+    try {
+        if (graph == nullptr) {
+            throw Missing("Input graph cannot be a nullptr.");
+        }
+        delete graph;
+    } catch (const std::exception &excpt) {
+        std::cout << excpt.what() << std::endl;
     }
-    delete graph;
 }
 
 Graph *addVertex(Graph *graph, char *v) {
-    if (graph == nullptr) {
-        throw Missing("Input graph cannot be a nullptr.");
+    try {
+        if (graph == nullptr) {
+            throw Missing("Input graph cannot be a nullptr.");
+        }
+        graph->addNode(v);
+        return graph;
+    } catch (const std::exception &excpt) {
+        std::cout << excpt.what() << std::endl;
+        return NULL;
     }
-    graph->addNode(v);
-    return graph;
 }
 
 Graph *addEdge(Graph *graph, char *v1, char *v2) {
-    if (graph == nullptr) {
-        throw Missing("Input graph cannot be a nullptr.");
+    try {
+        if (graph == nullptr) {
+            throw Missing("Input graph cannot be a nullptr.");
+        }
+        graph->addEdge(v1, v2);
+        return graph;
+    } catch (const std::exception &excpt) {
+        std::cout << excpt.what() << std::endl;
+        return NULL;
     }
-    graph->addEdge(v1, v2);
-    return graph;
 }
 
 Graph *graphUnion(Graph *graph_in1, Graph *graph_in2, Graph *graph_out) {
-    if (graph_in1 == nullptr or graph_in2 == nullptr or graph_out == nullptr) {
-        throw Missing("Input graph cannot be a nullptr.");
+    try {
+        if (graph_in1 == nullptr or graph_in2 == nullptr or
+            graph_out == nullptr) {
+            throw Missing("Input graph cannot be a nullptr.");
+        }
+        *graph_out = *graph_in1 + *graph_in2;
+        return graph_out;
+    } catch (const std::exception &excpt) {
+        std::cout << excpt.what() << std::endl;
+        return NULL;
     }
-    *graph_out = *graph_in1 + *graph_in2;
-    return graph_out;
 }
 
 Graph *graphIntersection(Graph *graph_in1, Graph *graph_in2, Graph *graph_out) {
-    if (graph_in1 == nullptr or graph_in2 == nullptr or graph_out == nullptr) {
-        throw Missing("Input graph cannot be a nullptr.");
+    try {
+        if (graph_in1 == nullptr or graph_in2 == nullptr or
+            graph_out == nullptr) {
+            throw Missing("Input graph cannot be a nullptr.");
+        }
+        *graph_out = *graph_in1 ^ *graph_in2;
+        return graph_out;
+    } catch (const std::exception &excpt) {
+        std::cout << excpt.what() << std::endl;
+        return NULL;
     }
-    *graph_out = *graph_in1 ^ *graph_in2;
-    return graph_out;
 }
 
 Graph *graphDifference(Graph *graph_in1, Graph *graph_in2, Graph *graph_out) {
-    if (graph_in1 == nullptr or graph_in2 == nullptr or graph_out == nullptr) {
-        throw Missing("Input graph cannot be a nullptr.");
+    try {
+        if (graph_in1 == nullptr or graph_in2 == nullptr or
+            graph_out == nullptr) {
+            throw Missing("Input graph cannot be a nullptr.");
+        }
+        *graph_out = *graph_in1 - *graph_in2;
+        return graph_out;
+    } catch (const std::exception &excpt) {
+        std::cout << excpt.what() << std::endl;
+        return NULL;
     }
-    *graph_out = *graph_in1 - *graph_in2;
-    return graph_out;
 }
 
 Graph *graphProduct(Graph *graph_in1, Graph *graph_in2, Graph *graph_out) {
-    if (graph_in1 == nullptr or graph_in2 == nullptr or graph_out == nullptr) {
-        throw Missing("Input graph cannot be a nullptr.");
+    try {
+        if (graph_in1 == nullptr or graph_in2 == nullptr or
+            graph_out == nullptr) {
+            throw Missing("Input graph cannot be a nullptr.");
+        }
+        *graph_out = (*graph_in1) * (*graph_in2);
+        return graph_out;
+    } catch (const std::exception &excpt) {
+        std::cout << excpt.what() << std::endl;
+        return NULL;
     }
-    *graph_out = (*graph_in1) * (*graph_in2);
-    return graph_out;
 }
 
 Graph *graphComplement(Graph *graph_in, Graph *graph_out) {
-    if (graph_in == nullptr or graph_out == nullptr) {
-        throw Missing("Input graph cannot be a nullptr.");
+    try {
+        if (graph_in == nullptr or graph_out == nullptr) {
+            throw Missing("Input graph cannot be a nullptr.");
+        }
+        *graph_out = !*graph_in;
+        return graph_out;
+    } catch (const std::exception &excpt) {
+        std::cout << excpt.what() << std::endl;
+        return NULL;
     }
-    *graph_out = !*graph_in;
-    return graph_out;
 }
 // }  // namespace GraphCalc
